d50af5b714bddb18900daa58331ded605ad6dd7e
yanfengqili <792615477@qq.com>
2020-07-15 17:47:16 +0800

    覆写 树形结构的 toTree() 方法

--- app/Admin/Repositories/Category.php
+++ app/Admin/Repositories/Category.php
@@ -15,4 +15,24 @@ class Category extends EloquentRepository implements TreeRepository
      */
     protected $eloquentClass = CategoryModel::class;
 
+    public function toTree()
+    {
+        $tree = parent::toTree();
+
+        return $this->getDeep($tree);
+    }
+
+    //  给树形结构加层级深度
+    protected function getDeep(&$tree, $deep = 1)
+    {
+        foreach ($tree as $k => &$row) {
+            $row['deep'] = $deep;
+
+            if(isset($row['children'])){
+                $this->getDeep($row['children'], $deep + 1);
+            }
+        }
+
+        return $tree;
+    }
 }
